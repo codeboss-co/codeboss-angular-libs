d4acfa135877b2145bcfb374b2d59b06e2daa4fc
Dillan Cagnetta <dillancagnetta@yahoo.com>
2021-09-14 15:27:11 +0200

    Fix: mat generic table library by regenerating

--- projects/mat-generic-table/karma.conf.js
+++ projects/mat-generic-table/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/codeboss/mat-generic-table'),
+      dir: require('path').join(__dirname, '../../coverage/mat-generic-table'),
       subdir: '.',
       reporters: [
         { type: 'html' },
